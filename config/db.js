c944e8074c7cc792f9865de24ac22564c90929db
Sradha Patro <sradha.ptr010@gmail.com>
2024-01-27 07:29:03 +0000

    connect to database initiated

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+
+const connectDB = async() => {    try{
+        mongoose.set("strictQuery",true);
+        await mongoose.connect(process.env.DB_URI,
+            {useNewUrlParser:true,
+            }).then(() =>{
+                console.log("successfully connetced to mongoDB")
+            })
+    } catch(err){
+        console.error(err.message)
+        process.exit(1);
+    }
+};
+module.exports=connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const  express=require("express")
+const  express=require("express");
+const connectDB= require("./config/db");
+const cors=require("cors");
+//connecting to mongodb
+connectDB();
 //Initiating the app
-const app=express()
+const app=express();
+//enabling cors here for safety and  for accesibility information from other details
+app.use(cors());
+//handle JSON parsing properly
+app.use(express.json())
 const port=process.env.PORT || 5000
 app.listen(port, () => 
 console.log(`Server is running on port ${port}`)
